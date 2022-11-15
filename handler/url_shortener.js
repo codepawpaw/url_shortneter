c350776e8bbc55388c7291c8f1923684432a8e90
Jonathan Natanael <js.jonathan.n@gmail.com>
2022-11-15 21:58:58 +0700

    wip 2

--- handler/url_shortener.js
+++ handler/url_shortener.js
@@ -1,5 +1,10 @@
 const RestResponder = require('../helpers/rest_responder');
 const { ShortenUrl } = require("../sequelize");
+const shortid = require('shortid')
+
+const rate_limiter_hash = {
+
+}
 
 module.exports.get = async (req, res) => {
   try {
@@ -13,15 +18,38 @@ module.exports.get = async (req, res) => {
 
     const modifiedUrl = req.params.modified_url;
 
+    const limitData = rate_limiter_hash[modifiedUrl]
+
+    if (limitData && limitData > 3) {
+      res.status(403).json({});
+      return;
+    }
+    
+    if (!limitData) {
+      rate_limiter_hash[modifiedUrl] = 1
+    } else {
+      if (limitData && limitData > 3) {
+        res.status(403).json({});
+        return;
+      }
+      rate_limiter_hash[modifiedUrl] = limitData + 1
+    }
+
     const data = await ShortenUrl.findOne({
       where: {
         modified_url: modifiedUrl
       }
     });
 
+    if (!data) {
+      res.status(404).json({});
+
+      return;
+    }
+
     const response = {
       original_url: data.original_url
-    }
+    };
 
     res.status(200).json(response);
 
@@ -40,8 +68,31 @@ module.exports.post = async (req, res) => {
         'Access-Control-Allow-Headers':
           'Origin, X-Requested-With, Content-Type, Accept',
       });
+
+      const {
+        original_url
+      } = req.body;
+
+      const existingData = await ShortenUrl.findOne({
+        where: {
+          original_url: original_url
+        }
+      });
+
+      if (existingData) {
+        res.status(200).json({});
+        return;
+      }
+
+      let urlCode = shortid.generate();
+      let shortUrl = "shortify" + "/" + urlCode;
+
+      await ShortenUrl.create({
+        original_url: original_url,
+        modified_url: shortUrl
+      });
   
-      RestResponder.success(res, 200, {}, '');
+      res.status(200).json({});
     } catch (error) {
       RestResponder.serverError(res, 500, error);
     }
--- tests/url_shortener.test.js
+++ tests/url_shortener.test.js
@@ -11,7 +11,7 @@ describe('Shorten Url API', () => {
   });
 
   describe('#get', () => {
-    it('should return shorten url when ', async () => {
+    it('given modified url should return original url', async () => {
       const stubUrl = "stub-modified-url"
 
       await ShortenUrl.create({
@@ -25,10 +25,96 @@ describe('Shorten Url API', () => {
 
       const expectedData = {
         original_url: "stub-original-url"
-      }
+      };
 
       expect(res.statusCode).toEqual(200)
       expect(res.body).toEqual(expectedData)
+    });
+
+    it('given not exists modified url should return 404 ', async () => {
+      const stubUrl = "stub-modified-url"
+
+      const res = await request(app)
+        .get(`/api/url_shorteners/${stubUrl}`)
+        .send()
+
+      expect(res.statusCode).toEqual(404);
     })
+
+    it('given 3 request should return 403 in the 3rd request', async () => {
+      const stubUrl = "stub-modified-url"
+
+      await ShortenUrl.create({
+        original_url: "stub-original-url",
+        modified_url: stubUrl
+      });
+
+      const res = await request(app)
+        .get(`/api/url_shorteners/${stubUrl}`)
+        .send()
+
+      const expectedData = {
+        original_url: "stub-original-url"
+      };
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual(expectedData)
+
+      //===================
+      const res2 = await request(app)
+        .get(`/api/url_shorteners/${stubUrl}`)
+        .send()
+      expect(res2.statusCode).toEqual(200)
+      expect(res2.body).toEqual(expectedData)
+
+      //===================
+      const res3 = await request(app)
+        .get(`/api/url_shorteners/${stubUrl}`)
+        .send()
+      expect(res3.statusCode).toEqual(403)
+    })
+  });
+
+  describe('#post', () => {
+    it('given valid data should save modified url to database', async () => {
+      const res = await request(app)
+        .post(`/api/url_shorteners`)
+        .send({
+          original_url: 'stub-original-url'
+        });
+
+      const expectedData = await ShortenUrl.findOne({
+        where: {
+          original_url: 'stub-original-url'
+        }
+      });
+
+      expect(res.statusCode).toEqual(200);
+      expect(expectedData.original_url).toEqual('stub-original-url');
+    });
+
+    it('given duplicate original url should not save multiple original url to database', async () => {
+      await ShortenUrl.create({
+        original_url: 'stub-original-url',
+        modified_url: 'stub-modified-url'
+      })
+
+      const res = await request(app)
+        .post(`/api/url_shorteners`)
+        .send({
+          original_url: 'stub-original-url'
+        });
+
+      const expectedData = await ShortenUrl.findAll({
+        where: {
+          original_url: 'stub-original-url'
+        }
+      });
+
+      expect(res.statusCode).toEqual(200);
+      expect(expectedData.length).toEqual(1);
+      expect(expectedData[0].original_url).toEqual('stub-original-url');
+      expect(expectedData[0].modified_url).toEqual('stub-modified-url');
+    });
   });
 })
